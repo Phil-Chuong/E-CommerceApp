968c1eb9fd40124bb693259f143583498e203363
Phil-Chuong <Phil-Chuong@users.noreply.github.com>
2024-08-13 17:14:43 +0100

    testing

--- backend/index.js
+++ backend/index.js
@@ -112,7 +112,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/public', 'index.html'));
 });
 
 
