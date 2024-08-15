00c7fdd27a78c2315191adbdfbc13df2ad9b83ef
Phil-Chuong <Phil-Chuong@users.noreply.github.com>
2024-08-15 17:37:33 +0100

    test28

--- backend/index.js
+++ backend/index.js
@@ -63,9 +63,11 @@ app.use((req, res, next) => {
 });
 
 // Serve static files from the "uploads" directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-//console.log(path.join(__dirname, 'uploads', 'your-image-file.jpg'));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use((req, res, next) => {
+  res.status(404).send('File not found');
+});
+console.log(path.join(__dirname, '/uploads', 'your-image-file.jpg'));
 
 // Initialize Stripe
 const stripe = Stripe(process.env.STRIPE_KEY);
