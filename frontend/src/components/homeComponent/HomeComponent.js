5fc5eb73cef4d5a9d95600b1a0bd53f41cba3601
Phil-Chuong <Phil-Chuong@users.noreply.github.com>
2024-08-16 11:19:50 +0100

    test

--- frontend/src/components/homeComponent/HomeComponent.js
+++ frontend/src/components/homeComponent/HomeComponent.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './HomeComponent.css';
 
+const imageURL = `https://techtitan.onrender.com`;
 
 function HomeComponent() {
 
@@ -38,7 +39,7 @@ function HomeComponent() {
                     <li key={product.id} className='homeProductItems'>
                         <Link to={`/products/${product.id}`}>
                             <h3>{product.name}</h3>
-                            <img src={product.image_path} alt={product.name} />
+                            <img src={`${imageURL}${product.image_path}`} alt={product.name} />
                             <p>£{product.price}</p>
                         </Link>
                     </li>
--- frontend/src/components/productDetailComponent/ProductDetailComponent.js
+++ frontend/src/components/productDetailComponent/ProductDetailComponent.js
@@ -4,7 +4,7 @@ import './ProductDetailComponent.css';
 import { useParams } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 
-
+const imageURL = `https://techtitan.onrender.com`;
 
 function ProductDetailComponent() {
 
@@ -163,7 +163,7 @@ function ProductDetailComponent() {
       <div className='productDetailContainer'>
 
         <div className="productDetailCard">
-          <img src={product.image_path} alt={product.name} className="product-image" />
+          <img src={`${imageURL}${product.image_path}`} alt={product.name} className="product-image" />
           
           <div className='productDetailInfo'>
             <h2>{product.name}</h2>
